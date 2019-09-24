c18b696bf8d4b4b0b9660f1caf24362e010e217c
Pradyumna Chatterjee <pady92@gmail.com>
2019-09-24 11:18:47 +0530

    message

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ call wenv\scripts\activate
 
 REM Upgrade pip in virtual env
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements-w_v.txt
 
 REM Windows Wheel Binary installer
 pip install https://download.lfd.uci.edu/pythonlibs/g5apjq5m/lxml-4.4.1-cp37-cp37m-win_amd64.whl
